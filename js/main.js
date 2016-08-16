00c164648a07c7056993462dea5f38280e038f72
Merle Fernandez Ancalla <mfernandez.a25@gmail.com>
2016-08-16 08:09:23 -0500

    avance

--- js/main.js
+++ js/main.js
@@ -44,47 +44,15 @@ $(".hover-notify").velocity('stop', true).velocity("fadeOut");
 });
 
 
+
+
+
+
+
+
 /* Demo purposes only */
 $("figure").mouseleave(
   function() {
     $(this).removeClass("hover");
   }
 );
-
-
-// menu
-$(window).scroll(function() {
-
-    if ($(window).scrollTop() > 100) {
-        $('.main_h').addClass('sticky');
-    } else {
-        $('.main_h').removeClass('sticky');
-    }
-});
-
-// Mobile Navigation
-$('.mobile-toggle').click(function() {
-    if ($('.main_h').hasClass('open-nav')) {
-        $('.main_h').removeClass('open-nav');
-    } else {
-        $('.main_h').addClass('open-nav');
-    }
-});
-
-$('.main_h li a').click(function() {
-    if ($('.main_h').hasClass('open-nav')) {
-        $('.navigation').removeClass('open-nav');
-        $('.main_h').removeClass('open-nav');
-    }
-});
-
-// navigation scroll lijepo radi materem
-$('nav a').click(function(event) {
-    var id = $(this).attr("href");
-    var offset = 70;
-    var target = $(id).offset().top - offset;
-    $('html, body').animate({
-        scrollTop: target
-    }, 500);
-    event.preventDefault();
-});
