62854647789982777a6916746ba98b7893cf1bc6
Merle Fernandez Ancalla <mfernandez.a25@gmail.com>
2016-08-22 21:05:07 -0500

    menu scroll

--- js/main.js
+++ js/main.js
@@ -58,6 +58,17 @@ $(window).scroll(function() {
     }
 });
 
+// navigation scroll lijepo radi materem
+$('.click').click(function(event) {
+    var id = $(this).attr("href");
+    var offset = 70;
+    var target = $(id).offset().top - offset;
+    $('html, body').animate({
+        scrollTop: target
+    }, 500);
+    event.preventDefault();
+});
+
 
 /* Demo purposes only */
 $(".hover").mouseleave(
